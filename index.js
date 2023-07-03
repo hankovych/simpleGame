67ad1f8b682f439daa1f5ca89663c9dbafabbb0a
nadiia-hankovich <hankovich.nadiia@gmail.com>
2023-07-03 11:37:27 +0300

    added js files and styles

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>simpleGame</title>
-</head>
-<body>
+const playerChoice = document.getElementById('playerChs');
+const computerChoice = document.getElementById('computerChs');
+const resultIss = document.getElementById('resultText');
+const choiceButton = document.querySelectorAll('.choiceBtn');
+
+let player;
+let computer;
+let result;
+
+choiceButton.forEach(choiceButton =>
+    choiceButton.addEventListener('click', (e) => {
+        player = e.target.id;
+        player = choiceButton.innerHTML;
+        computerTurn();
+        playerChoice.innerHTML = `Player: ${player}`;
+        computerChoice.innerHTML = `Computer: ${computer}`;
+        resultIss.innerHTML = winnerCheck();
     
-</body>
-</html>
+    }));
+
+function computerTurn() {
+    const randomNumb = Math.floor((Math.random() * choiceButton.length) + 1);
+    console.log(randomNumb);
+    switch(randomNumb) {
+        case 1:
+            computer = 'Rock';
+            break;
+            case 2:
+                computer = 'Scissors';
+            break;
+            case 2:
+                computer = 'Paper';
+            break;
+        
+    }
+}
+
+function winnerCheck() {
+    if (player == computer) {
+        return "Draw!";
+    }
+    else if (computer == 'Rock') {
+        return (player == 'Paper') ? "You win!": "You lose";
+    }
+    else if (computer == 'Paper') {
+        return (player == 'Scissors') ? "You win!" : "You lose";
+    }
+    else if(computer == 'Scissors') {
+        return(player== 'Rock') ? "You win!" : "You lose";
+    }
+}
